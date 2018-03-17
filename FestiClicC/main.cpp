721f23027142a9b3e2e4e453c180496da1653650
Mourad <mourad.zammit@gmail.com>
2018-03-17 19:30:54 +0100

    Image button style sheet

--- FestiClicC/main.cpp
+++ FestiClicC/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Accueil w;
+    //image de fond page Accueil
+    w.setStyleSheet("background-image:url(:spactacle2.jpg)");
     w.show();
 
     return a.exec();
